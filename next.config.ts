f66acfda845d8f81315d8baa4309099d2a88fe7d
JenilK04 <kovadiyajenil11@gmail.com>
2026-06-10 14:08:47 +0530

    changing deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",       // Compiles down to standard static HTML/CSS/JS
-  images: {
-    unoptimized: true,    // Required for static export to load public assets properly
-  },
-  basePath: "/portfolio",   // Injects your exact GitHub repository path
-  assetPrefix: "/portfolio/", // Forces subfolders to fetch assets from the correct relative path
+  /* config options here */
 };
 
 export default nextConfig;
